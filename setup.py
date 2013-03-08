9f9bce1a60ae2fb0baf19aa9accca2a8e99219c5
Baptiste Mille-Mathias <baptistem@gnome.org>
2013-03-08 18:18:55 +0100

    white spaces clean up in setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from distutils.core import setup
 
-with open('README.md') as f:
+with open('README') as f:
     long_description = f.read()
 
 setup(name='bayfiles',
-        version='0.0.1',
-        description='A library to upload files to bayfiles.com',
-        long_description=long_description,
-        py_modules=['bayfiles'],
-        author='Baptiste Mille-Mathias',
-        author_email='baptiste.millemathias@gmail.com',
-        url='https://github.com/bmillemathias/python-bayfiles',
-        license='LGPL',
-        classifiers=['Development Status :: 2 - Pre-Alpha',
+        version = '0.0.1',
+        description = 'A library to upload files to bayfiles.com',
+        long_description = long_description,
+        py_modules = ['bayfiles'],
+        author = 'Baptiste Mille-Mathias',
+        author_email = 'baptiste.millemathias@gmail.com',
+        url = 'https://github.com/bmillemathias/python-bayfiles',
+        license = 'LGPL',
+        classifiers = ['Development Status :: 2 - Pre-Alpha',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
             'Operating System :: POSIX',
